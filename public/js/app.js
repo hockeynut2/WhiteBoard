1b9748e3b45facd833ef7befb61662983f84c532
zkornbluth <zk49844@students.westport.k12.ct.us>
2015-06-03 20:36:27 -0400

    update index.erb, app.js, add main.css

--- public/js/app.js
+++ public/js/app.js
@@ -20,19 +20,53 @@ function downloadImg() {
     window.location.href = img;
 }
 
-$(document).ready(function() {
-    $(".dropdown-toggle").click(function(ev) {
-        $(".dropdown-toggle").dropdown("toggle");
-        return false;
-    });
-    $("ul.dropdown-menu a").click(function(ev) {
-        $("a.dropdown-toggle").dropdown("toggle");
-        return false;
-    });
-});
+//
+//$('li a.opt').click(function(){
+//    var value = $(this).text();
+//    $('a.dropdown-toggle').html(value);
+//    $('a.dropdown-toggle').append('<span class="caret">'+'</span>');
+//});
 
-$('li a').click(function(){
-    var value = $(this).text();
-    $('a.dropdown-toggle').html(value);
-    $('a.dropdown-toggle').append('<span class="caret">'+'</span>');
-});
+function drawDot(brush, x,y) {
+    // Let's use black by setting RGB values to 0, and 255 alpha (completely opaque)
+    r=0; g=0; b=0; a=255;
+
+    // Select a fill style
+    brush.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
+    // Draw a filled circle
+    brush.beginPath();
+    brush.arc(x, y, size, 0, Math.PI*2, true);
+    brush.closePath();
+    brush.fill();
+}
+
+var mouseX, mouseY, mouseDown = 0;
+function canvas_mouseDown() {
+    mouseDown = 1;
+    drawDot(brush, mouseX, mouseY)
+}
+
+function canvas_mouseUp() {
+    mouseDown = 0;
+}
+
+function canvas_mouseMove(e) {
+    getMousePos(e);
+    if (mouseDown == 1) {
+        drawDot(brush, mouseX, mouseY)
+    }
+}
+
+function getMousePos(e) {
+    if (e.offsetX) {
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+    } else if (e.layerX) {
+        mouseX = e.layerX;
+        mouseY = e.layerY;
+    }
+}
+
+canvas.addEventListener('mousedown', canvas_mouseDown, false);
+canvas.addEventListener('mousemove', canvas_mouseMove, false);
+window.addEventListener('mouseup', canvas_mouseUp, false);
