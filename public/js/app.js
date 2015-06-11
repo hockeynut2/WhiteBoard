c265f4797f472faaa123d4481f84fbc6306b26c8
zkornbluth <zk49844@students.westport.k12.ct.us>
2015-06-11 10:32:28 -0400

    Update index.erb, app.js with rectangles

--- public/js/app.js
+++ public/js/app.js
@@ -48,18 +48,188 @@ brush.lineCap = 'round';
 brush.strokeStyle = 'black';
 
 /* Event listener work */
-canvas.addEventListener('mousedown', function(e) {
+
+function onPaint() {
+    brush.lineTo(mouse.x, mouse.y);
+    brush.stroke();
+}
+
+function paintStart() {
     brush.beginPath();
     brush.moveTo(mouse.x, mouse.y);
 
     canvas.addEventListener('mousemove', onPaint, false);
-}, false);
+}
 
-canvas.addEventListener('mouseup', function() {
-    canvas.removeEventListener('mousemove', onPaint, false);
-}, false);
+/* Marker on/off */
 
-var onPaint = function() {
-    brush.lineTo(mouse.x, mouse.y);
-    brush.stroke();
-};
+function marker() {
+    document.getElementById('white').removeEventListener('click', fillWhite);
+    document.getElementById('red').removeEventListener('click', fillRed);
+    document.getElementById('orange').removeEventListener('click', fillOrange);
+    document.getElementById('yellow').removeEventListener('click', fillYellow);
+    document.getElementById('green').removeEventListener('click', fillGreen);
+    document.getElementById('blue').removeEventListener('click', fillBlue);
+    document.getElementById('purple').removeEventListener('click', fillPurple);
+    document.getElementById('black').removeEventListener('click', fillBlack);
+
+    document.getElementById('white').addEventListener('click', colorWhite);
+    document.getElementById('red').addEventListener('click', colorRed);
+    document.getElementById('orange').addEventListener('click', colorOrange);
+    document.getElementById('yellow').addEventListener('click', colorYellow);
+    document.getElementById('green').addEventListener('click', colorGreen);
+    document.getElementById('blue').addEventListener('click', colorBlue);
+    document.getElementById('purple').addEventListener('click', colorPurple);
+    document.getElementById('black').addEventListener('click', colorBlack);
+
+    $('#canvas').unbind('mousedown');
+
+    canvas.addEventListener('mousedown', paintStart);
+
+    canvas.addEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+}
+
+/* Rectangle work */
+var isDrawing = false;
+var startX;
+var startY;
+
+function dynamicRect(e) {
+    var mouseX = parseInt(e.clientX - canvas.offsetLeft);
+    var mouseY = parseInt(e.clientY - canvas.offsetTop);
+    if (isDrawing) {
+        isDrawing = false;
+        brush.beginPath();
+        brush.rect(startX, startY, mouseX - startX, mouseY - startY);
+        brush.fill();
+        brush.stroke();
+        canvas.style.cursor = "default";
+    } else {
+        isDrawing = true;
+        startX = mouseX;
+        startY = mouseY;
+        canvas.style.cursor = "crosshair";
+    }
+}
+function rectangleDrawer() {
+    document.getElementById('white').removeEventListener('click', colorWhite);
+    document.getElementById('red').removeEventListener('click', colorRed);
+    document.getElementById('orange').removeEventListener('click', colorOrange);
+    document.getElementById('yellow').removeEventListener('click', colorYellow);
+    document.getElementById('green').removeEventListener('click', colorGreen);
+    document.getElementById('blue').removeEventListener('click', colorBlue);
+    document.getElementById('purple').removeEventListener('click', colorPurple);
+    document.getElementById('black').removeEventListener('click', colorBlack);
+
+    document.getElementById('white').addEventListener('click', fillWhite);
+    document.getElementById('red').addEventListener('click', fillRed);
+    document.getElementById('orange').addEventListener('click', fillOrange);
+    document.getElementById('yellow').addEventListener('click', fillYellow);
+    document.getElementById('green').addEventListener('click', fillGreen);
+    document.getElementById('blue').addEventListener('click', fillBlue);
+    document.getElementById('purple').addEventListener('click', fillPurple);
+    document.getElementById('black').addEventListener('click', fillBlack);
+
+    canvas.removeEventListener('mousedown', function(e) {
+        brush.beginPath();
+        brush.moveTo(mouse.x, mouse.y);
+
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
+
+    canvas.removeEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+
+
+    brush.strokeStyle = 'black';
+    brush.lineWidth = 5;
+    $('#canvas').mousedown(function (e) {
+        dynamicRect(e);
+    });
+}
+
+/* Color changers */
+function colorRed() {
+    brush.strokeStyle = 'red';
+    brush.lineWidth = 5;
+}
+
+function colorOrange() {
+    brush.strokeStyle = 'orange';
+    brush.lineWidth = 5;
+}
+
+function colorYellow() {
+    brush.strokeStyle = 'yellow';
+    brush.lineWidth = 5;
+}
+
+function colorGreen() {
+    brush.strokeStyle = 'green';
+    brush.lineWidth = 5;
+}
+
+function colorBlue() {
+    brush.strokeStyle = 'blue';
+    brush.lineWidth = 5;
+}
+
+function colorPurple() {
+    brush.strokeStyle = 'purple';
+    brush.lineWidth = 5;
+}
+
+function colorBlack() {
+    brush.strokeStyle = 'black';
+    brush.lineWidth = 5;
+}
+
+function colorWhite() {
+    brush.strokeStyle = 'white';
+    brush.lineWidth = 5;
+}
+
+/* Eraser */
+
+function colorEraser() {
+    brush.strokeStyle = 'white';
+    brush.lineWidth = 50;
+}
+
+marker();
+
+/* Rectangle fillers */
+function fillRed() {
+    brush.fillStyle = 'red';
+}
+
+function fillOrange() {
+    brush.fillStyle = 'orange';
+}
+
+function fillYellow() {
+    brush.fillStyle = 'yellow';
+}
+
+function fillGreen() {
+    brush.fillStyle = 'green';
+}
+
+function fillBlue() {
+    brush.fillStyle = 'blue';
+}
+
+function fillPurple() {
+    brush.fillStyle = 'purple';
+}
+
+function fillBlack() {
+    brush.fillStyle = 'black';
+}
+
+function fillWhite() {
+    brush.fillStyle = 'white';
+}
